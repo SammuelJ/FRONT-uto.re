e1209345d08b8548194db172266d5863e1e73108
SammuelJ <sammueljnssns@gmail.com>
2020-07-02 08:13:33 +0200

    Less is more:

--- src/TolongURL.js
+++ src/TolongURL.js
@@ -1,26 +1,5 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-
-class TolongURL extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: "undefined",
-        };
-    }
-    componentWillMount() {
-        const slug = window.location.pathname.substr(1);
-        fetch("https://utore.herokuapp.com/" + slug)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.url) {
-                    window.location.href = data.url;
-                }
-            });
-    }
-    render() {
-        return <Redirect to={{ pathname: "/", search: "?m=0" }} />;
-    }
-}
-
+const TolongURL = async () => {
+    window.location.href =
+        "https://utore.herokuapp.com/" + window.location.pathname.substr(1);
+};
 export default TolongURL;
